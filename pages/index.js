f54aeb16dac537d5276dff7c65a97d1e80c7d56a
rezcode <rezharians@gmail.com>
2022-08-16 15:26:21 +0800

    [ADD] header component and slicing form input search flight

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import React from "react";
 import style from "../styles/Home.module.css";
 import Footer from "../component/footer";
+import { FiSearch } from "react-icons/fi";
+import HomeHeader from "../component/homeHeader";
 
 const Explore = () => {
   return (
     <>
       <div className="row justify-content-center">
-        <div className={`col-md-4 ${style.bodyWrapper}`}></div>
+        <div className={`col-md-4 ${style.bodyWrapper}`}>
+          <HomeHeader />
+          <div className="container">
+            <div id="inputSearchHome" className="row mt-3">
+              <div className="input-group input-group-lg">
+                <span className="input-group-text" id="inputGroup-sizing-lg">
+                  <FiSearch size={30} color="#A3A3A3" />
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-label="Sizing example input"
+                  aria-describedby="inputGroup-sizing-lg"
+                  placeholder="Where you want to go?"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     </>
