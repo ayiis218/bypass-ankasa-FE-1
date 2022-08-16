b9259a66765b8928d31fef530c5280cf48737fce
rezcode <rezharians@gmail.com>
2022-08-16 14:37:20 +0800

    [add] footer slicing

---
+++ component/footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import style from "../styles/Home.module.css";
+import { TbCalendarTime } from "react-icons/tb";
+import { RiCompass3Fill } from "react-icons/ri";
+import { CgProfile } from "react-icons/cg";
+
+const Footer = () => {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-4">
+        <div className={`row align-items-center ${style.footer}`}>
+          <div className="col-4 text-center">
+            <TbCalendarTime size={27} color="#979797" />
+            <br />
+            <small>My Booking</small>
+          </div>
+          <div className={`col-4 text-center ${style.goFlight}`}>
+            <RiCompass3Fill size={55} color="white" />
+          </div>
+          <div className="col-4 text-center">
+            <CgProfile size={28} color="#979797" />
+            <br />
+            <small>Profile</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
