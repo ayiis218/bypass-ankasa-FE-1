6538b1122b98c1037fd35d9699403efa39c833d4
rezcode <rezharians@gmail.com>
2022-08-19 00:14:23 +0800

    [FIX] footer home page

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,16 @@ import TopTenDestinations from "../component/topTenDestinations";
 const Explore = () => {
   return (
     <>
-      <div className={`row justify-content-center ${style.bodyWrapper}`}>
+      <div className={`row pt-4 justify-content-center ${style.bodyWrapper}`}>
         <div className={`col-md-4 p-0 ${style.exploreWrapper}`}>
-          <div className="container">
+          <div className={`container ${style.fixHeight}`}>
             <Header title={"Explore"} />
-            <div className="container">
-              <div id="inputSearchHome" className="row mt-3">
+            <div className="container py-4">
+              <div id="inputSearchHome" className="row mt-4">
                 <div className="input-group input-group-lg">
                   <span className="input-group-text" id="inputGroup-sizing-lg">
                     <FiSearch size={30} color="#A3A3A3" />
                   </span>
-
                   <input
                     type="text"
                     className="form-control"
@@ -31,7 +30,9 @@ const Explore = () => {
                   />
                 </div>
               </div>
-              <TrendingDestinations />
+              <div className="py-4">
+                <TrendingDestinations />
+              </div>
               <TopTenDestinations />
             </div>
           </div>
