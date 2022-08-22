31ae8b2ef0d3b5ad1c92c20c7d5400ed92b4a8e1
aldoBangun <punk.aldo68@gmail.com>
2022-08-22 21:21:49 +0800

    udpate authslice

--- redux/features/authSlice.js
+++ redux/features/authSlice.js
@@ -1,13 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-// tolong hapus ini nanti
-// nilai yang return dari fungsi asyncThunk ini kemudian akan masuk ke action.payload
-// di extraReducer authSlice itu sudah saya lakukan destructuring dari object action sehingga bisa ...
-// ... langsung akses payload
 export const login = createAsyncThunk('auth/login', async (user, { rejectWithValue }) => {
   try {
-    const response = await axios.post('https://bypass-ankasa-backend.herokuapp.com/auth/login', user)
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, user)
     return response?.data?.token
   } catch(err) {
     return rejectWithValue(err?.response?.data?.message)
@@ -16,7 +12,7 @@ export const login = createAsyncThunk('auth/login', async (user, { rejectWithVal
 
 export const register = createAsyncThunk('auth/register', async(newUser, { rejectWithValue }) => {
   try {
-    const response = await axios.post('https://bypass-ankasa-backend.herokuapp.com/auth/register', newUser)
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, newUser)
     return response?.data?.message
   } catch(err) {
     if (err?.response?.status === 400) return rejectWithValue('User with this email already exist')
@@ -47,9 +43,6 @@ const authSlice = createSlice({
     }
   },
   extraReducers: {
-    // tolong ini dihapus nanti
-    // untuk tiap action yang ada di extra reducer ini punya 3 type yaitu:
-    // pending, fulfilled, & rejected
     [login.pending]: (state) => {
       state.isLoading = true
       state.token = null
